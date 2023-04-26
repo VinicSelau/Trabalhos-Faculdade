fb34294bd259b6fbcdecec9c4b1362c0e0d8a2b3
Vinicius Selau da Silva <85204902+VinicSelau@users.noreply.github.com>
2023-04-26 20:08:26 -0300

    Delete teste_banco directory

--- teste_banco/Pessoa.py
+++
@@ -1,9 +0,0 @@
-class Pessoa:
-    def __init__(self, nome, cpf, telefone, email, agencia, numero_conta, senha):
-        self.nome = nome
-        self.cpf = cpf
-        self.telefone = telefone
-        self.email = email
-        self.agencia = agencia
-        self.numero_conta = numero_conta
-        self.senha = senha
